4b958301a240a0670883514e7a049df78e484041
MMAZeem3 <musa.mazeem@gmail.com>
2022-07-05 17:21:18 -0400

    wrapped activity logging in a toggle button

--- app/src/main/java/com/example/delta/MainActivity.kt
+++ app/src/main/java/com/example/delta/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.delta
 import android.app.Activity
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
 import android.hardware.SensorManager
+import android.hardware.SensorEventListener
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
 import android.widget.ToggleButton
 import com.example.delta.databinding.ActivityMainBinding
+import java.io.FileOutputStream
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var f: FileOutputStream
+    private val samplingRateHertz = 100
+    private val samplingRateMicroseconds = samplingRateHertz*100
+    private var mAccel: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,13 +27,44 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        f = this.openFileOutput("out.csv", Context.MODE_PRIVATE)
+
         val toggle: ToggleButton = findViewById(R.id.activityToggleButton)
         toggle.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                Log.d("PRINT", "on")
+                onStartButton()
+                Log.i("0001", "on")
             } else {
-                Log.d("PRINT", "off")
+                onStopButton()
+                Log.i("0001", "off")
             }
         }
+    }
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        Log.i("0001", "Logging")
+        f.write((event.timestamp.toString()+","+
+                event.values[0].toString()+","+
+                event.values[1].toString()+","+
+                event.values[2].toString()+"\n").toByteArray())
+    }
+    private fun onStartButton() {
+        Log.i("0001", "Start")
+        f = this.openFileOutput("out.csv", Context.MODE_PRIVATE)
+        f.write("timestamp,x,y,z,test,est\n".toByteArray())
+        mAccel?.also { accel ->
+            sensorManager.registerListener(this, accel,
+                samplingRateMicroseconds, samplingRateMicroseconds)
+        }
+    }
+
+    private fun onStopButton() {
+        Log.i("0001", "Stop")
+        f.close()
+        sensorManager.unregisterListener(this)
     }
 }
