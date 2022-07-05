a0748efe53b60452331895de515afd6ef9ba6e63
MMAZeem3 <musa.mazeem@gmail.com>
2022-07-05 15:07:52 -0400

    first commit on branch

---
+++ app/src/main/java/com/example/delta/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.delta
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.delta.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
