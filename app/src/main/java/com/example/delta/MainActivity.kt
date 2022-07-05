f4e55c800e66f9c45dedfe30a6d9e3a47051e293
MMAZeem3 <musa.mazeem@gmail.com>
2022-07-05 15:46:32 -0400

    added toggle button

--- app/src/main/java/com/example/delta/MainActivity.kt
+++ app/src/main/java/com/example/delta/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.delta
 
 import android.app.Activity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
 import com.example.delta.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -14,5 +21,13 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toggle: ToggleButton = findViewById(R.id.activityToggleButton)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Log.d("PRINT", "on")
+            } else {
+                Log.d("PRINT", "off")
+            }
+        }
     }
 }
