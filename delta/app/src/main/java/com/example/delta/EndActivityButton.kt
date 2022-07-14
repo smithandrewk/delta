57de2b4adf3c5043b3fccf4c5824067fa6e4b183
MMAZeem3 <musa.mazeem@gmail.com>
2022-07-14 15:50:33 -0400

    progress bar working

--- delta/app/src/main/java/com/example/delta/EndActivityButton.kt
+++ delta/app/src/main/java/com/example/delta/EndActivityButton.kt
@@ -1,17 +1,25 @@
 package com.example.delta
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.wear.widget.CurvedTextView
 
 
 class EndActivityButton : Activity() {
-    var cTimer: CountDownTimer? = null
-//    var progressBarAnswer =
+    private var cTimer: CountDownTimer? = null
+    private lateinit var progressBar: ProgressBar
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_button)
@@ -19,40 +27,50 @@ class EndActivityButton : Activity() {
         // stuff to display file and sampling frequency
         val samplingRateHertz = intent.getStringExtra("SamplingRateKey")
         val filename = intent.getStringExtra("FilenameKey")
-        var xmlSamplingFrequency: CurvedTextView = findViewById(R.id.samplingFrequency)
-        var xmlFilename: CurvedTextView = findViewById(R.id.filename)
+        val xmlSamplingFrequency: CurvedTextView = findViewById(R.id.samplingFrequency)
+        val xmlFilename: CurvedTextView = findViewById(R.id.filename)
         xmlSamplingFrequency.text = "$samplingRateHertz Hz"
         xmlFilename.text = filename
 
-        // when button is clicked, go back to main activity
-        findViewById<Button>(R.id.endActivityButton).setOnClickListener {
-            val returnIntent = Intent()
-            setResult(Activity.RESULT_OK, returnIntent)
-            finish()
+        progressBar = findViewById(R.id.progressBar)
+
+        findViewById<Button>(R.id.endActivityButton).setOnTouchListener { _, event -> //when button is pressed
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                //show the progressBar
+                progressBar.visibility = View.VISIBLE
+                startTimer()
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                progressBar.progress = 0
+                progressBar.visibility = View.INVISIBLE
+                cancelTimer()
+            }
+            true
         }
 
     }
 
-//    //start timer function
-//    fun startTimer() {
-//        cTimer = object : CountDownTimer(2000, 200) {
-//            override fun onTick(millisUntilFinished: Long) {
-//                progressBarAnswer.setProgress(progressBarAnswer.getProgress() + 10)
-//            }
-//
-//            override fun onFinish() {
-//                progressBarAnswer.setProgress(0)
-//                progressBarAnswer.setVisibility(View.INVISIBLE)
-//                showAnswer()
-//            }
-//        }
-//        cTimer.start()
-//    }
-//
-//    //cancel timer
-//    fun cancelTimer() {
-//        if (cTimer != null) cTimer!!.cancel()
-//    }
+    //start timer function
+    private fun startTimer() {
+        cTimer = object : CountDownTimer(2000, 20) {
+            override fun onTick(millisUntilFinished: Long) {
+                progressBar.progress = progressBar.progress + 1
+            }
+
+            override fun onFinish() {
+                progressBar.progress = 0
+                progressBar.visibility = View.INVISIBLE
+                val returnIntent = Intent()
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
+            }
+        }
+        (cTimer as CountDownTimer).start()
+    }
+
+    //cancel timer
+    private fun cancelTimer() {
+        if (cTimer != null) cTimer!!.cancel()
+    }
     override fun onDestroy() {
         super.onDestroy()
         Log.i("0002", "DESTROYED")
