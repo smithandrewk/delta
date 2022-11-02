a62fe15305efdf3f0fca9764f408e06fba2a3b30
Andrew <andrewsmith1025@gmail.com>
2022-11-02 11:22:15 -0400

    not-so-simplified

--- delta/app/src/main/java/com/example/delta/presentation/MainActivity.kt
+++ delta/app/src/main/java/com/example/delta/presentation/MainActivity.kt
@@ -1,69 +1,60 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.example.delta.presentation
 
-import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.ScalingLazyColumn
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.rememberScalingLazyListState
+import androidx.lifecycle.MutableLiveData
 import com.example.delta.R
-import com.example.delta.presentation.theme.DeltaTheme
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : ComponentActivity() {
-    private lateinit var accelIntent: Intent
-    private lateinit var dataFolderName: String
-    private lateinit var fPuffs: FileOutputStream
-    private lateinit var puffsFilename: String
-
+class MainActivity : ComponentActivity(), SensorEventListener {
     private val appStartTimeReadable = SimpleDateFormat("yyyy-MM-dd_HH_mm_ss", Locale.ENGLISH).format(Date())
+    private lateinit var dataFolderName: String
+    private lateinit var fRaw: FileOutputStream
+    private lateinit var fFalseNegatives: FileOutputStream
+
+    private lateinit var rawFilename: String
+    private lateinit var fSession: FileOutputStream
+    private lateinit var sessionFilename: String
+    private val numWindowsBatched = 1
+    private var rawFileIndex: Int = 0
+    private var currentActivity: String = "None"
+    private lateinit var nHandler: NeuralHandler
+    private lateinit var falseNegativesFilename: String
+    private lateinit var falseNegativesFile: File
+    private var userDeclaredTheyAreSmoking: Boolean = false
+    private val liveData = MutableLiveData<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp(this)
+            WearApp(this,liveData)
         }
-//        createFile()
+        createInitialFiles()
+        createAndRegisterAccelerometerListener()
+        nHandler = getNeuralHandler()
 
-        // Start Accelerometer Service
-        // start service to record accelerometer data
-//        accelIntent = Intent(applicationContext, AccelLoggerService::class.java)
-//            .putExtra("StartTime", appStartTimeReadable)
-//        startForegroundService(accelIntent)
     }
-    private fun createFile(){
-        dataFolderName = appStartTimeReadable
-        puffsFilename = "Smoking-Sessions.$dataFolderName.csv"
-        fPuffs = FileOutputStream(File(this.filesDir, "$dataFolderName/$puffsFilename"))
-        fPuffs.use { f ->
-            f.write("Start Time,Stop Time\n".toByteArray())
-        }
+    private fun createAndRegisterAccelerometerListener() {
+        // Register Listener for Accelerometer Data
+        val samplingRateHertz = 100
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val samplingPeriodMicroseconds = 1000000/samplingRateHertz
+        sensorManager.registerListener(this, sensor, samplingPeriodMicroseconds)
     }
-
     fun onSmokeToggle(clicked: Boolean) {
         if (clicked) {
+            sendSessionDetectedConfirmationDialog()
             // Session start
             Log.i("0001", "Start")
         }
@@ -71,43 +62,104 @@ class MainActivity : ComponentActivity() {
             Log.i("0001", "Stop")
         }
     }
-}
-
-@Composable
-fun WearApp(instance: MainActivity) {
-    DeltaTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
+    override fun onSensorChanged(event: SensorEvent) {
+        /*
+            We observed experimentally Ticwatch E samples at 100 Hz consistently for 9 hours
+            in our app. Therefore, we take every 5th value from onsensorchanged to approximate
+            20 Hz sampling rate.
          */
-        val listState = rememberScalingLazyListState()
+        writeToRawFile("${event.timestamp},${event.values[0]},${event.values[1]},${event.values[2]}")
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // do nothing
+    }
+    private fun createInitialFiles(){
+        currentActivity = getString(R.string.NO_ACTIVITY)
 
-        val contentModifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 8.dp)
-        val iconModifier = Modifier
-            .size(24.dp)
-            .wrapContentSize(align = Alignment.Center)
+        // Create folder for this session's files
+        dataFolderName = appStartTimeReadable
+        File(this.filesDir, dataFolderName).mkdir()
+        createNewRawFile()
+        createFalseNegativesFile()
+        // Create session file and first raw file
+        sessionFilename = "Session.$appStartTimeReadable.csv"    // file to save session information
+        fSession = FileOutputStream(File(this.filesDir, "$dataFolderName/$sessionFilename"))
+        writeToSessionFile("File Start Time: ${Calendar.getInstance().timeInMillis}\n")
+        writeToSessionFile("Event,Start Time,Stop Time\n")
 
-        ScalingLazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(
-                top = 64.dp,
-                start = 8.dp,
-                end = 8.dp,
-                bottom = 32.dp
-            ),
-            verticalArrangement = Arrangement.Center,
-            state = listState
-        ) {
-            item { SelfReportChip(contentModifier, iconModifier) }
-            item { SessionStartToggleButton(contentModifier, instance) }
+        val fInfo = FileOutputStream(File(this.filesDir, "$dataFolderName/Info.txt"))
+        fInfo.use { f ->
+            f.write("Number of Windows in each Batch: $numWindowsBatched".toByteArray())
         }
     }
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp(MainActivity())
+    private fun writeToSessionFile(str: String) {
+        // write a string to the session file
+        Log.i("0003", "Writing to Session File")
+        fSession = FileOutputStream(File(this.filesDir, "$dataFolderName/$sessionFilename"), true)
+        fSession.use { f ->
+            f.write(str.toByteArray())
+        }
+    }
+    private fun writeToRawFile(str: String){
+        fRaw.write("${str}\n".toByteArray())
+    }
+    private fun createNewRawFile() {
+        // Create a new raw file for accelerometer data
+        Log.i("0003", "Creating New Raw File")
+        if (rawFileIndex != 0) {
+            fRaw.close()
+        }
+        rawFilename = "$appStartTimeReadable.$rawFileIndex.csv"       // file to save raw data
+        fRaw = FileOutputStream(File(this.filesDir, "$dataFolderName/$rawFilename"))
+        fRaw.write("File Start Time: ${Calendar.getInstance().timeInMillis}\n".toByteArray())
+        fRaw.write("timestamp,acc_x,acc_y,acc_z,real time,activity,label,state\n".toByteArray())
+        rawFileIndex++
+    }
+    private fun getNeuralHandler(): NeuralHandler{
+        // Load ANN weights and input ranges
+        // TODO: Can we move loading the weights to the NeuralHandler class?
+        var ins: InputStream = resources.openRawResource(R.raw.input_to_hidden_weights_and_biases)
+        val inputToHiddenWeightsAndBiasesString = ins.bufferedReader().use { it.readText() }
+        ins.close()
+        ins = resources.openRawResource(R.raw.hidden_to_output_weights_and_biases)
+        val hiddenToOutputWeightsAndBiasesString = ins.bufferedReader().use { it.readText() }
+        ins.close()
+        ins = resources.openRawResource(R.raw.input_ranges)
+        val inputRangesString = ins.bufferedReader().use { it.readText() }
+        ins.close()
+        return NeuralHandler(
+            "andrew",
+            inputToHiddenWeightsAndBiasesString,
+            hiddenToOutputWeightsAndBiasesString,
+            inputRangesString,
+            numWindowsBatched,applicationContext)
+    }
+    fun onReportFalseNegative(){
+        falseNegativesFile.appendText("${Calendar.getInstance().timeInMillis}\n")
+    }
+    private fun createFalseNegativesFile(){
+        falseNegativesFilename = "False-Negatives.$dataFolderName.csv"
+        falseNegativesFile = File(this.filesDir, "$dataFolderName/$falseNegativesFilename")
+        fFalseNegatives = FileOutputStream(falseNegativesFile)
+        fFalseNegatives.use { f ->
+            f.write("Time\n".toByteArray())
+        }
+    }
+    fun onSessionDetectedByModel(){
+        // TODO
+        if(userDeclaredTheyAreSmoking){
+            /*
+                if user declared they are smoking,
+                then don't send dialog
+             */
+        } else {
+            sendSessionDetectedConfirmationDialog()
+        }
+    }
+    private fun sendSessionDetectedConfirmationDialog(){
+        liveData.postValue(true)
+    }
+    fun setLiveDataFalse(){
+        liveData.postValue(false)
+    }
 }
--- delta/app/src/main/java/com/example/delta/presentation/ReusableComponents.kt
+++ delta/app/src/main/java/com/example/delta/presentation/ReusableComponents.kt
@@ -1,26 +1,65 @@
 package com.example.delta.presentation
 
 import android.util.Log
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import com.example.delta.R
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
 import androidx.wear.compose.material.*
+import androidx.wear.compose.material.dialog.Alert
+import androidx.wear.compose.material.dialog.Dialog
+import com.example.delta.presentation.theme.DeltaTheme
 
+@Composable
+fun WearApp(instance: MainActivity,liveData: LiveData<Boolean>) {
+    DeltaTheme {
+        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+         * version of LazyColumn for wear devices with some added features. For more information,
+         * see d.android.com/wear/compose.
+         */
+        val listState = rememberScalingLazyListState()
+
+        val contentModifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+        val iconModifier = Modifier
+            .size(24.dp)
+            .wrapContentSize(align = Alignment.Center)
+
+        ScalingLazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                top = 64.dp,
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 32.dp
+            ),
+            verticalArrangement = Arrangement.Center,
+            state = listState
+        ) {
+            item { SelfReportChip(contentModifier, iconModifier, instance) }
+            item { SessionStartToggleButton(contentModifier, instance) }
+            item { ShowDialogExample(liveData,instance)}
+        }
+    }
+}
 @Composable
 fun SelfReportChip (
     modifier: Modifier = Modifier,
-    iconModifier: Modifier = Modifier
+    iconModifier: Modifier = Modifier,
+    instance: MainActivity
 ) {
     Chip(
         modifier = modifier,
-        onClick = { /* Log smoking to session */ },
+        onClick = { instance.onReportFalseNegative() },
         label = {
             Text(
                 text = "I just smoked dummy",
@@ -67,5 +106,60 @@ fun SessionStartToggleButton(
 //            contentDescription = "smoking icon",
 //            modifier = iconModifier,
 //        )
+    }
+}
+
+
+
+@Composable
+fun ShowDialogExample(show: LiveData<Boolean>,instance: MainActivity){
+    val scrollState = rememberScalingLazyListState()
+    val showDialog: Boolean? by show.observeAsState()
+
+    showDialog?.let {
+        Dialog(
+            showDialog = it,
+            onDismissRequest = { instance.setLiveDataFalse() },
+            scrollState = scrollState
+        ) {
+            Alert(
+                scrollState = scrollState,
+                verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.Top),
+                contentPadding =
+                PaddingValues(start = 10.dp, end = 10.dp, top = 24.dp, bottom = 52.dp),
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.lungs),
+                        contentDescription = "airplane",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .wrapContentSize(align = Alignment.Center),
+                    )
+                },
+                title = { Text(text = "Example Title Text", textAlign = TextAlign.Center) },
+                message = {
+                    Text(
+                        text = "Message content goes here",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.body2
+                    )
+                },
+            ) {
+                item {
+                    Chip(
+                        label = { Text("Primary") },
+                        onClick = { instance.setLiveDataFalse()  },
+                        colors = ChipDefaults.primaryChipColors(),
+                    )
+                }
+                item {
+                    Chip(
+                        label = { Text("Secondary") },
+                        onClick = { instance.setLiveDataFalse()  },
+                        colors = ChipDefaults.secondaryChipColors(),
+                    )
+                }
+            }
+        }
     }
 }
